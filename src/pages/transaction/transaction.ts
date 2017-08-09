8b4b36a30a05cb2e72dc300fc4963fb3331d08d7
Devin Shoemaker <devinshoe@gmail.com>
2017-08-09 18:56:46 -0500

    Transaction: add basic profit calculator

--- src/pages/transaction/transaction.ts
+++ src/pages/transaction/transaction.ts
@@ -14,11 +14,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TransactionPage {
 
+  quantity: number;
+  purchasePrice: number;
+  suggestedSellPrice: number;
+  purchaseDollars: number;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TransactionPage');
   }
+
+
+  calculateSuggestedSalePrice() {
+    if (this.purchaseDollars && this.purchasePrice) {
+      this.quantity = this.purchaseDollars / this.purchasePrice;
+    }
+
+    if (this.purchasePrice && this.quantity) {
+      this.purchaseDollars = this.purchasePrice * this.quantity;
+    }
+
+    let percentageFee = this.purchaseDollars * 0.0149;
+
+    let purchaseFlatFee = 2.99;
+    let purchaseFee = 0.0;
+    if (percentageFee > percentageFee) {
+      purchaseFee = percentageFee;
+    } else {
+      purchaseFee = purchaseFlatFee;
+    }
+
+    let sellersFlatFee = 1.49;
+    let sellersFee = 0.0;
+    if (percentageFee > sellersFlatFee) {
+      sellersFee = percentageFee;
+    } else {
+      sellersFee = sellersFlatFee;
+    }
+
+    this.suggestedSellPrice = (Number(this.purchasePrice) + purchaseFee + sellersFee) * 1.02;
+  }
+
 
 }
