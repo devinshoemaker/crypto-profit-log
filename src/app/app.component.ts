aac7de7dde03ad96722a7e5cdc2dfe245e7961c3
Devin Shoemaker <devinshoe@gmail.com>
2017-08-09 18:58:16 -0500

    Revert "Set 'Transaction' as the root page"
    This reverts commit 64314ee2f420236b4682718a15f70abda646c6d1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-// import { HomePage } from '../pages/home/home';
-import { TransactionPage } from '../pages/transaction/transaction';
+import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TransactionPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -20,3 +19,4 @@ export class MyApp {
     });
   }
 }
+
