630ad9fa2a3f66fd6d361c87e5366d749b1717c1
Devin Shoemaker <devinshoe@gmail.com>
2017-08-19 08:47:58 -0500

    TransactionProvider: Transaction CRUD

--- src/providers/transaction/transaction.ts
+++ src/providers/transaction/transaction.ts
@@ -29,4 +29,71 @@ export class TransactionProvider {
     this.db.sync(this.remote, options);
   }
 
+  getAllTransactions() {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+      this.db.allDocs({
+        include_docs: true
+      }).then((result) => {
+        this.data = [];
+
+        result.rows.map((row) => {
+          this.data.push(row.doc);
+        });
+
+        resolve(this.data);
+
+        this.db.changes({live: true, since: 'now', include_docs:true}).on('change', (change) => {
+          this.handleChange(change);
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+  }
+
+  createTransaction(transaction) {
+    this.db.post(transaction);
+  }
+
+  updateTransaction(transaction) {
+    this.db.put(transaction).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  deleteTransaction(transaction) {
+    this.db.remove(transaction).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  handleChange(change) {
+    let changedDoc = null;
+    let changedIndex = null;
+
+    this.data.forEach((doc, index) => {
+      if (doc._id === change.id) {
+        changedDoc = doc;
+        changedIndex = index;
+      }
+    });
+
+    // A document was deleted
+    if (change.deleted) {
+      this.data.splice(changedIndex, 1);
+    } else {
+      // A document was updated or added
+      if (changedDoc) {
+        this.data[changedIndex] = change.doc;
+      } else {
+        this.data.push(change.doc);
+      }
+
+    }
+  }
+
 }
